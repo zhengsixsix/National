3d895a7a85cb35a58e66ce0f395ad010ad3d42d0
1314520zjX! <1833496831@qq.com>
2022-11-03 19:03:57 +0800

    解决线上无法查看代码bug

--- src/components/preview.vue
+++ src/components/preview.vue
@@ -41,22 +41,11 @@ const showOrhidecode = () => {
 };
 const sourceCode = ref("");
 async function getSourceCode() {
-  const isDev = import.meta.env.MODE === "development";
-  if (isDev) {
     sourceCode.value = (
       await import(
         /* @vite-ignore */ `/src/packages/${props.compname}/doc/${props.demoname}.vue?raw`
-      )    
+      )
     ).default;
-  } else {
-    await fetch(
-      `/src/packages/${props.compname}/doc/${props.demoname}.vue`
-    ).then((res) => {
-      res.text().then(a=>{
-        sourceCode.value = a
-      })
-    });
-  }
 }
 onMounted(() => {
   getSourceCode();
